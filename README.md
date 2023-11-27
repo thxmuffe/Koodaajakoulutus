998e012ee4b706080707bb0fec76135e246ee2a2
Paavo Penttila <thxmuffe@hotmail.com>
2023-11-27 14:00:48 +0200

    Create PersonsController automatically with EF-tools

--- README.md
+++ README.md
@@ -19,7 +19,17 @@ CREATE TABLE Persons (
 
 
 WebAPI:
+Voit yksinkertaistaa launch-settings tiedostoa, jättää ainoastaan https-version, jolloin vähemmän epäselvyttää mikä profiili käytössä. Voit testata WeatherController contolleria esim: https://localhost:7183/WeatherForecast
+
 kts. https://learn.microsoft.com/en-us/ef/core/cli/dotnet
 Tee EF-model aiemman osan tietokannasta.
 ConnectionString:n saa kopioitua Azuresta, ja voit sen jälkeen luoda mallin:
 dotnet ef dbcontext scaffold komennolla.
+
+Kun sinulla on EF-malli luoto, lisää uusi kontroller ohjelmaasi, joka tallettaa uuden henkilön persons kantaan. Tai hakee listan Persons taulussa olevista henkilöistä.
+
+Kontrolleri kannattaa tehdä käsin, mutta voit myös aluksi tehdä sen automaattisesti komentoriviltä, jolloin näet millainen siitä pitäisi tulla. kts. https://learn.microsoft.com/en-us/aspnet/core/fundamentals/tools/dotnet-aspnet-codegenerator?view=aspnetcore-8.0
+
+dotnet aspnet-codegenerator --project . controller -name YOUR_CONTROLLER_NAME -m YOUR_MODEL_NAME -dc YOUR_DB_CONTEXT_CLASS -outDir Controllers/
+
+esim: dotnet aspnet-codegenerator --project . controller -name PersonsController -m Person -dc FreeAzureSqlContext -outDir Controllers/
---
+++ WebAPI/Controllers/PersonsController.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class PersonsController : Controller
+    {
+        private readonly FreeAzureSqlContext _context;
+
+        public PersonsController(FreeAzureSqlContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Persons
+        public async Task<IActionResult> Index()
+        {
+              return _context.Persons != null ? 
+                          View(await _context.Persons.ToListAsync()) :
+                          Problem("Entity set 'FreeAzureSqlContext.Persons'  is null.");
+        }
+
+        // GET: Persons/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons
+                .FirstOrDefaultAsync(m => m.Personid == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // GET: Persons/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Persons/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Personid,LastName,FirstName,Age")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        // GET: Persons/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        // POST: Persons/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Personid,LastName,FirstName,Age")] Person person)
+        {
+            if (id != person.Personid)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.Personid))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        // GET: Persons/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons
+                .FirstOrDefaultAsync(m => m.Personid == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // POST: Persons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Persons == null)
+            {
+                return Problem("Entity set 'FreeAzureSqlContext.Persons'  is null.");
+            }
+            var person = await _context.Persons.FindAsync(id);
+            if (person != null)
+            {
+                _context.Persons.Remove(person);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonExists(int id)
+        {
+          return (_context.Persons?.Any(e => e.Personid == id)).GetValueOrDefault();
+        }
+    }
+}
