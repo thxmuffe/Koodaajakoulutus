bc66f83ee463f7bc103be6f88eb6569ac949daf0
Paavo Penttila <thxmuffe@hotmail.com>
2023-12-11 10:06:01 +0200

    (temporarily) remove controller

--- WebAPI/Models/Person.cs
+++ WebAPI/Models/Person.cs
@@ -7,9 +7,9 @@ public partial class Person
 {
     public int Personid { get; set; }
 
-    public string LastName { get; set; } = null!;
+    public string LastName { get; set; }
 
-    public string? FirstName { get; set; }
+    public string FirstName { get; set; }
 
-    public int? Age { get; set; }
+    public int Age { get; set; }
 }
